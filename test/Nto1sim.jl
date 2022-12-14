2f6666a50010f82d64f72a1f02b4186641f15852
Tomas Fiers <tomas.fiers@gmail.com>
2022-12-14 15:53:18 +0000

    Nto1sim: annot params. and found bug in diffeqs :o (`Δ.u` ~ `(v-vₗ)` but was `-vᵣ`. Not reset, but rest (leak)).
    this was not in Voltage-to-wiring-sim code luckily (so never made it into a notebook :))

--- test/Nto1sim.jl
+++ test/Nto1sim.jl
@@ -2,66 +2,65 @@ using Firework
 using Firework.Units
 using Firework: LogNormal
 
-# Parameters
+# Neuron model params
 @typed begin
-    # Izhikevich neuron
-    C  =  100    * pF
-    k  =    0.7  * (nS/mV)
-    vₗ = - 60    * mV
-    vₜ = - 40    * mV
-    a  =    0.03 / ms
-    b  = -  2    * nS
-    vₛ =   35    * mV
-    vᵣ = - 50    * mV
-    Δu =  100    * pA
-    # Synapses
-    Eₑ =   0 * mV
-    Eᵢ = -80 * mV
-    τ  =   7 * ms
-    # Integration
-    Δt = 0.1ms
-    T  = 10seconds
+    # Izhikevich params
+    C  =  100    * pF        # Cell capacitance
+    k  =    0.7  * (nS/mV)   # Steepness of parabola in v̇(v)
+    vₗ = - 60    * mV        # Resting ('leak') membrane potential
+    vₜ = - 40    * mV        # Spiking threshold (when no syn. & adaptation currents)
+    a  =    0.03 / ms        # Reciprocal of time constant of adaptation current `u`
+    b  = -  2    * nS        #
+    vₛ =   35    * mV        # Spike cutoff (defines spike time)
+    vᵣ = - 50    * mV        # Reset voltage after spike
+    Δu =  100    * pA        # Adaptation current inflow on self-spike
+    # Conductance-based synapses
+    Eₑ =   0 * mV            # Reversal potential at excitatory synapses
+    Eᵢ = -80 * mV            # Reversal potential at inhibitory synapses
+    τ  =   7 * ms            # Time constant for synaptic conductances' decay
 end
-
-# Conductance-based Izhikevich neuron
-g_izh_neuron = NeuronModel(
-
+coba_izh_neuron = NeuronModel(  # "Conductance-based Izhikevich neuron model"
     # Simulated variables, and their initial values
-    (
-        v   =  vᵣ,
-        u   = 0 * pA,
-        gₑ  = 0 * nS,
-        gᵢ  = 0 * nS,
+    x₀ = (
+        # Izhikevich variables
+        v   =  vᵣ,     # Membrane potential
+        u   = 0 * pA,  # Adaptation current
+        # Synaptic conductances g
+        gₑ  = 0 * nS,  # = Sum over all exc. synapses
+        gᵢ  = 0 * nS,  # = Sum over all inh. synapses
     ),
-
-    # Set time derivatives (Δ) of simulated vars
-    (Δ, vars) -> begin
+    # Differential equations: provide time derivatives (in `Δ`) of simulated vars
+    diffeqs = (Δ, vars) -> begin
         @unpack v, u, gₑ, gᵢ = vars
 
         # Conductance-based synaptic current
         Iₛ = gₑ*(v-Eₑ) + gᵢ*(v-Eᵢ)
+
         # Izhikevich 2D system
-        Δ.v = (k*(v-vₗ)*(v-vₜ) - u - Iₛ) / C  # Membrane potential
-        Δ.u = a*(b*(v-vᵣ) - u)                # Adaptation current
+        Δ.v = (k*(v-vₗ)*(v-vₜ) - u - Iₛ) / C
+        Δ.u = a*(b*(v-vₗ) - u)
+
         # Synaptic conductance decay
-        # (gₑ is sum over all exc synapses, gᵢ over all inh)
         Δ.gₑ = -gₑ / τ
         Δ.gᵢ = -gᵢ / τ
     end,
-
-    # Spike condition
-    (vars) -> (vars.v ≥ vₛ),
-
-    # On-self-spike
-    (vars) -> begin
+    spiking_condition = (vars) -> (vars.v ≥ vₛ),
+    on_self_spike = (vars) -> begin
         vars.v = vᵣ
         vars.u += Δu
     end
 )
 
+# Simulation
+@typed begin
+    Δt = 0.1ms      # Sim timestep
+    T  = 10seconds  # Sim length ('recording duration')
+end
+
 
 # Inputs
 
+# Params
 @typed begin
     Nₑ = 40
     Nᵢ = 10
@@ -87,7 +86,9 @@ fr_distr = LogNormal(median = 4Hz, g = 2)
 λs = rand(fr_distr, N)
 spiketrains = [poisson_spiketrain(λ, T) for λ in λs]
 
-m = Nto1Model(spiketrains, on_spike_arrival, g_izh_neuron)
+m = Nto1Model(spiketrains, on_spike_arrival, coba_izh_neuron)
+
+
 
 
 using Firework: CVector
