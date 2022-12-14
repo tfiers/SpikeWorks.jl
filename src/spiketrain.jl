7bba53846e7977f05ffa5258d56d8e78ac4dd071
Tomas Fiers <tomas.fiers@gmail.com>
2022-12-14 15:24:36 +0000

    rm `Base.merge(spiketrains)` (we have SpikeMux now, and keeping it around would be confusing)

--- src/spiketrain.jl
+++ src/spiketrain.jl
@@ -28,22 +28,3 @@ duration(t::SpikeTrain) = t.duration
 
 numspikes(t::SpikeTrain) = length(t)
 spikerate(t::SpikeTrain) = numspikes(t) / duration(t)
-
-Base.merge(trains::AbstractVector{SpikeTrain}) =
-    SpikeTrain(
-        merge_sorted(spiketimes.(trains)),
-        maximum(duration, trains);
-        checksorted = false,
-    )
-
-merge_sorted(vecs) = sort!(reduce(vcat, vecs))
-# ↪ This implementation does not explicitly make use of the fact that the vecs are already
-#   sorted. But quicksort performs well here. Much better than a specific implementation of
-#   `merge_sorted` I wrote (see commit a0bc3c8 / this file and <test/merge_sorted.jl>).
-#
-#   Note that `reduce(vcat, vecs)` is efficient. It doesn't do the default reduce thing,
-#   where it would iteratively allocate larger and larger arrays and repeatedly copy over
-#   the same data. Rather, there is a `reduce` method specialized on `::typeof(vcat)` that
-#   allocates the final big array in one go. (Specifically, see `_typed_vcat(::Type{T},
-#    V::AbstractVecOrTuple{AbstractVector})` in `abstractarray.jl`).
-#   (The use of `reduce` for non-reduce functionality is unintuitive, Julia Base designers).
